16a0730b9e8c470e01df609345bc4c0fb118a76c
diluisi <luiz.diego5@gmail.com>
2016-06-29 21:19:39 -0300

    Atualização do ui.R
    Atualização do ui.R

--- ui.R
+++ ui.R
@@ -8,26 +8,49 @@
 #
 
 library(shiny)
+library(shinydashboard)
+library(RColorBrewer)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
-  )
-))
+shinyUI(fluidPage(theme = "bootstrap.css",
+                  
+                  tags$head(
+                    tags$style(HTML("
+                                    @import url('//fonts.googleapis.com/css?family=Lobster|Cabin:400,700');
+                                    
+                                    h1 {
+                                    font-family: 'Lobster', cursive;
+                                    font-weight: 500;
+                                    line-height: 1.1;
+                                    color: #48ca3b;
+                                    }
+                                    
+                                    "))
+                    ),
+                  dashboardPage(
+                    dashboardHeader(),
+                    dashboardSidebar(),
+                    dashboardBody()
+                  ),
+                  
+                  headerPanel("Uncapacitated Facility Location Problem"),
+                  
+                  # Application title
+                  titlePanel("Old Faithful Geyser Data"),
+                  
+                  # Sidebar with a slider input for number of bins 
+                  sidebarLayout(
+                    sidebarPanel(
+                      sliderInput("bins",
+                                  "Number of bins:",
+                                  min = 1,
+                                  max = 50,
+                                  value = 30)
+                    ),
+                    
+                    # Show a plot of the generated distribution
+                    mainPanel(
+                      plotOutput("distPlot")
+                    )
+                  )
+                    ))
